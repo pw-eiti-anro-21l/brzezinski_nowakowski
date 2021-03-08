a99a5007584236247f9b812c635ad48c7b138081
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-03-08 13:15:30 +0100

    Żółw się teraz kręci w kółko

--- ANRO_Turtlesim/ANRO_Turtlesim/ANRO_Turtlesim_control.py
+++ ANRO_Turtlesim/ANRO_Turtlesim/ANRO_Turtlesim_control.py
@@ -1,5 +1,39 @@
-def main():
-    print('Hi from ANRO_Turtlesim.')
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+import geometry_msgs.msg #import Twist
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('ANRO_Turtlesim_control')
+        self.publisher_ = self.create_publisher(geometry_msgs.msg.Twist, '/turtle1/cmd_vel', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = geometry_msgs.msg.Twist()
+        msg.linear.x = 1.0
+        msg.angular.z = 0.6
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing')
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
