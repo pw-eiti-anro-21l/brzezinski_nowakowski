7c0361812d8809a8178107496f83903fac5684a5
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-03-09 19:48:50 +0100

    Żółw się kręci tylko podczas trzymania przycisku p

--- ANRO_Turtlesim/ANRO_Turtlesim/ANRO_Turtlesim_control.py
+++ ANRO_Turtlesim/ANRO_Turtlesim/ANRO_Turtlesim_control.py
@@ -1,32 +1,36 @@
+import getch
+
 import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
 import geometry_msgs.msg #import Twist
-
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('ANRO_Turtlesim_control')
         self.publisher_ = self.create_publisher(geometry_msgs.msg.Twist, '/turtle1/cmd_vel', 10)
-        timer_period = 0.5  # seconds
+        timer_period = 0.02  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
         msg = geometry_msgs.msg.Twist()
-        msg.linear.x = 1.0
-        msg.angular.z = 0.6
+        key = getch.getch()
+        if  key == 'p':
+            msg.linear.x = 1.0
+            msg.angular.z = 0.6
+        else:
+            msg.linear.x = 0.0
+            msg.angular.z = 0.0
+            self.get_logger().info('key code: %d' % ord(key))
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing')
         self.i += 1
 
 
 def main(args=None):
     rclpy.init(args=args)
-
     minimal_publisher = MinimalPublisher()
-
     rclpy.spin(minimal_publisher)
 
     # Destroy the node explicitly
