bed7477b4ccb6fdb70ec79d425ea24cdda5a8b91
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-03-11 00:33:27 +0100

    Zmieniono wykrywanie wciśnięcia klawiszy (getch) na wersję nieblokującą (wykorzystujący  kbhit)

--- ANRO_Turtlesim/ANRO_Turtlesim/ANRO_Turtlesim_control.py
+++ ANRO_Turtlesim/ANRO_Turtlesim/ANRO_Turtlesim_control.py
@@ -1,4 +1,4 @@
-import getch
+import os
 
 import rclpy
 import rclpy.node
@@ -8,12 +8,111 @@ from rcl_interfaces.msg import ParameterType
 from std_msgs.msg import String
 import geometry_msgs.msg #import Twist
 
+
+# Windows
+if os.name == 'nt':
+    import msvcrt
+
+# Posix (Linux, OS X)
+else:
+    import sys
+    import termios
+    import atexit
+    from select import select
+
+
+class KBHit:
+
+    def __init__(self):
+        '''Creates a KBHit object that you can call to do various keyboard things.
+        '''
+
+        if os.name == 'nt':
+            pass
+
+        else:
+
+            # Save the terminal settings
+            self.fd = sys.stdin.fileno()
+            self.new_term = termios.tcgetattr(self.fd)
+            self.old_term = termios.tcgetattr(self.fd)
+
+            # New terminal setting unbuffered
+            self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
+
+            # Support normal-terminal reset at exit
+            atexit.register(self.set_normal_term)
+
+
+    def set_normal_term(self):
+        ''' Resets to normal terminal.  On Windows this is a no-op.
+        '''
+
+        if os.name == 'nt':
+            pass
+
+        else:
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
+
+
+    def getch(self):
+        ''' Returns a keyboard character after kbhit() has been called.
+            Should not be called in the same program as getarrow().
+        '''
+
+        s = ''
+
+        if os.name == 'nt':
+            return msvcrt.getch().decode('utf-8')
+
+        else:
+            return sys.stdin.read(1)
+
+
+    def getarrow(self):
+        ''' Returns an arrow-key code after kbhit() has been called. Codes are
+        0 : up
+        1 : right
+        2 : down
+        3 : left
+        Should not be called in the same program as getch().
+        '''
+
+        if os.name == 'nt':
+            msvcrt.getch() # skip 0xE0
+            c = msvcrt.getch()
+            vals = [72, 77, 80, 75]
+
+        else:
+            c = sys.stdin.read(3)[2]
+            vals = [65, 67, 66, 68]
+
+        return vals.index(ord(c.decode('utf-8')))
+    def  purging_getch(self):
+        key = chr(0)
+        while kb.kbhit():
+            key = kb.getch()
+        return key
+
+
+    def kbhit(self):
+        ''' Returns True if keyboard character was hit, False otherwise.
+        '''
+        if os.name == 'nt':
+            return msvcrt.kbhit()
+
+        else:
+            dr,dw,de = select([sys.stdin], [], [], 0)
+            return dr != []
+kb = KBHit()
+
 class MinimalPublisher(rclpy.node.Node):
 
     def __init__(self):
         super().__init__('anro_turtlesim_control')
         self.publisher_ = self.create_publisher(geometry_msgs.msg.Twist, '/turtle1/cmd_vel', 10)
-        timer_period = 0.02  # seconds
+        timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.declare_parameter('forward', 'w')
         self.declare_parameter('backward', 's')
@@ -27,8 +126,8 @@ class MinimalPublisher(rclpy.node.Node):
         left = self.get_parameter('left').get_parameter_value().string_value
         right = self.get_parameter('right').get_parameter_value().string_value
         msg = geometry_msgs.msg.Twist()
-        key = getch.getch()
         
+        key = kb.purging_getch()
         if  key == forward:
             msg.linear.x = 1.0
             msg.angular.z = 0.0
@@ -41,6 +140,9 @@ class MinimalPublisher(rclpy.node.Node):
         elif key == right:
             msg.linear.x = 0.0
             msg.angular.z = 1.0
+        else:
+            msg.linear.x = 0.0
+            msg.angular.z = 0.0
         
         #my_new_param = rclpy.parameter.Parameter(
         #    'my_parameter',
