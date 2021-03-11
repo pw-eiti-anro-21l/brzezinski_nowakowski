d6f902f5c4fd6da67cc97a3e903ad5f44aa7f2cc
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-03-11 14:37:27 +0100

    Dodano roslaunch, który  uruchamia turtlesim i turtlesim_control w oddzielnej konsoli

---
+++ anro_turtlesim/launch/turtlesim_control.launch.py
@@ -0,0 +1,35 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            # namespace='turtlesim1',
+            executable='turtlesim_node',
+            name='sim'
+        ),
+        # Node(
+        #     package='anro_turtlesim',
+        #     executable='turtlesim_control',
+        #     name='control',
+        #     output='screen'
+        # )
+        Node(
+            package='turtlesim',
+            executable='turtle_teleop_key',
+            name='control',
+            # output='screen',
+            prefix=['xterm -e ']
+        )
+    ])
+# <launch>
+
+#  <!-- Turtlesim Node-->
+#   <node pkg="turtlesim" type="turtlesim_node" name="turtlesim"/>
+
+#  <!-- Axes -->
+#   <node pkg="anro_turtlesim" type="turtlesim_control" name="turtlesim_control" output="screen"/>
+
+# </launch>
--- anro_turtlesim/setup.py
+++ anro_turtlesim/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'anro_turtlesim'
@@ -10,8 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'launch'],
     zip_safe=True,
     maintainer='gabriel',
     maintainer_email='gabriel@todo.todo',
