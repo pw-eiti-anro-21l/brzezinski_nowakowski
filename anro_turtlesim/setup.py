947519caa54583c15bc662e3fc9a025d4dcbbc4d
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-03-15 01:01:17 +0100

    Zaktualizowano package.xml i setup.py

--- anro_turtlesim/setup.py
+++ anro_turtlesim/setup.py
@@ -6,7 +6,7 @@ package_name = 'anro_turtlesim'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -14,12 +14,12 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
-    install_requires=['setuptools', 'launch'],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='gabriel',
-    maintainer_email='gabriel@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Gabriel Brzeziński, Kacper Nowakowski',
+    maintainer_email='gabrysbrzezinski@gmail.com, casperus99@wp.pl',
+    description='Simple node for controling turtlesim with keyboard',
+    license='MIT License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
