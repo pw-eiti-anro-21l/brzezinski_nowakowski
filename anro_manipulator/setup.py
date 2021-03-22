4f266dc5a2ee0105f90c32f2d70ab4ad7d214ba4
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-03-22 12:39:49 +0100

    Inicjalizacja urdf i rviz

--- anro_manipulator/setup.py
+++ anro_manipulator/setup.py
@@ -1,4 +1,7 @@
+import os
+from glob import glob
 from setuptools import setup
+from setuptools import find_packages
 
 package_name = 'anro_manipulator'
 
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name), glob('urdf/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,6 +25,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'state_publisher = anro_manipulator.state_publisher:main'
         ],
     },
 )
