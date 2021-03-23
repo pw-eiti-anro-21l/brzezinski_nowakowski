a6191f5b777afb935ec69b08f641dc2ce3495cdb
Casperus99 <80170154+Casperus99@users.noreply.github.com>
2021-03-23 13:05:51 +0100

    Update state_publisher.py

--- anro_manipulator/anro_manipulator/state_publisher.py
+++ anro_manipulator/anro_manipulator/state_publisher.py
@@ -23,12 +23,12 @@ class StatePublisher(Node):
             loop_rate = self.create_rate(30)
 
             # robot state
-            tilt = 0.
-            tinc = degree
-            swivel = 0.
-            angle = 0.
-            height = 0.
-            hinc = 0.005
+            #tilt = 0.
+            #tinc = degree
+            #swivel = 0.
+            #angle = 0.
+            #height = 0.
+            #hinc = 0.005
 
             # message declarations
             odom_trans = TransformStamped()
@@ -37,40 +37,41 @@ class StatePublisher(Node):
             joint_state = JointState()
 
             try:
-                    while rclpy.ok():
-                            rclpy.spin_once(self)
+                while rclpy.ok():
+                    #swivel += degree
+                    rclpy.spin_once(self)
 
-                            # update joint_state
-                            now = self.get_clock().now()
-                            joint_state.header.stamp = now.to_msg()
-                            # joint_state.name = ['swivel', 'tilt', 'periscope']
-                            # joint_state.position = [swivel, tilt, height]
+                    # update joint_state
+                    now = self.get_clock().now()
+                    joint_state.header.stamp = now.to_msg()
+                    #joint_state.name = ['base-cyl']
+                    #joint_state.position = [swivel]
 
-                            # update transform
-                            # (moving in a circle with radius=2)
-                            odom_trans.header.stamp = now.to_msg()
-                            # odom_trans.transform.translation.x = cos(angle)*2
-                            # odom_trans.transform.translation.y = sin(angle)*2
-                            # odom_trans.transform.translation.z = 0.7
-                            # odom_trans.transform.rotation = \
-                            #         euler_to_quaternion(0, 0, angle + pi/2) # roll,pitch,yaw
+                    # update transform
+                    # (moving in a circle with radius=2)
+                    # odom_trans.header.stamp = now.to_msg()
+                    # odom_trans.transform.translation.x = cos(angle)*2
+                    # odom_trans.transform.translation.y = sin(angle)*2
+                    # odom_trans.transform.translation.z = 0.7
+                    # odom_trans.transform.rotation = \
+                    #         euler_to_quaternion(0, 0, angle + pi/2) # roll,pitch,yaw
 
-                            # send the joint state and transform
-                            self.joint_pub.publish(joint_state)
-                            self.broadcaster.sendTransform(odom_trans)
+                    # send the joint state and transform
+                    self.joint_pub.publish(joint_state)
+                    self.broadcaster.sendTransform(odom_trans)
 
-                            # Create new robot state
-                            # tilt += tinc
-                            # if tilt < -0.5 or tilt > 0.0:
-                            #         tinc *= -1
-                            # height += hinc
-                            # if height > 0.2 or height < 0.0:
-                            #         hinc *= -1
-                            # swivel += degree
-                            # angle += degree/4
+                    # Create new robot state
+                    # tilt += tinc
+                    # if tilt < -0.5 or tilt > 0.0:
+                    #         tinc *= -1
+                    # height += hinc
+                    # if height > 0.2 or height < 0.0:
+                    #         hinc *= -1
+                    # swivel += degree
+                    # angle += degree/4
 
-                            # This will adjust as needed per iteration
-                            loop_rate.sleep()
+                    # This will adjust as needed per iteration
+                    loop_rate.sleep()
 
             except KeyboardInterrupt:
                     pass
