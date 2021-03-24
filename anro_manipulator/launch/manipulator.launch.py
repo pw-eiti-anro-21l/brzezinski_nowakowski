182911071bac30ae6fc85b7574f6a13a638a237f
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-03-24 13:02:58 +0100

    Added xacro for urdf generation

--- anro_manipulator/launch/manipulator.launch.py
+++ anro_manipulator/launch/manipulator.launch.py
@@ -2,6 +2,7 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.actions import ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -9,18 +10,23 @@ def generate_launch_description():
 
   use_sim_time = LaunchConfiguration('use_sim_time', default='false')
   urdf_file_name = 'manipulator.urdf.xml'
+  xacro_file_name = 'manipulator_fixed.xacro.xml'
 
   print("urdf_file_name : {}".format(urdf_file_name))
 
   urdf = os.path.join(
       get_package_share_directory('anro_manipulator'),
       urdf_file_name)
+  xacro = os.path.join(
+      get_package_share_directory('anro_manipulator'),
+      xacro_file_name)
 
   return LaunchDescription([
       DeclareLaunchArgument(
           'use_sim_time',
           default_value='false',
           description='Use simulation (Gazebo) clock if true'),
+      ExecuteProcess(cmd=['/bin/bash', '-c', 'xacro ' + xacro + ' -o ' + urdf]),
       Node(
           package='robot_state_publisher',
           executable='robot_state_publisher',
