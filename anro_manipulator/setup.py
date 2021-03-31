82e2ea82fcd23a8846d14eed6f5c8a6b7769c92c
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-04-01 01:11:54 +0200

    Added meshes for visual tags

--- anro_manipulator/setup.py
+++ anro_manipulator/setup.py
@@ -49,7 +49,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name), glob('urdf/*'))
+        (os.path.join('share', package_name), glob('urdf/*')),
+        (os.path.join('share', package_name), glob('meshes/*'))
     ],
     cmdclass={
         'xacro': XacroCommand,
