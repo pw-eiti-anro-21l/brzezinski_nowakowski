5443e4bbf38fd6ba2a2a9ff88b4e441fcd7e0b4b
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-04-10 17:37:02 +0200

    rviz.launch.file has 'fixed' argument

--- anro_manipulator/launch/rviz.launch.py
+++ anro_manipulator/launch/rviz.launch.py
@@ -5,17 +5,18 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
 
 from launch_ros.actions import Node
 
 
 def generate_launch_description():
 
+    fixed = LaunchConfiguration('fixed', default='false')
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
     rviz_file_name = 'manipulator.rviz'
     urdf_file_name = 'manipulator.urdf.xml'
-
-    print('urdf_file_name : {}'.format(urdf_file_name))
+    urdf_fixed_file_name = 'manipulator.fixed.urdf.xml'
 
     rviz = os.path.join(
             get_package_share_directory('anro_manipulator'),
@@ -23,10 +24,16 @@ def generate_launch_description():
     urdf = os.path.join(
             get_package_share_directory('anro_manipulator'),
             urdf_file_name)
-    print('rviz path: ' + rviz)
+    urdf_fixed = os.path.join(
+            get_package_share_directory('anro_manipulator'),
+            urdf_fixed_file_name)
     return LaunchDescription([
             DeclareLaunchArgument(
                     'use_sim_time',
+                    default_value='false',
+                    description='Use simulation (Gazebo) clock if true'),
+            DeclareLaunchArgument(
+                    'fixed',
                     default_value='false',
                     description='Use simulation (Gazebo) clock if true'),
             Node(
@@ -35,7 +42,18 @@ def generate_launch_description():
                     name='robot_state_publisher',
                     output='screen',
                     parameters=[{'use_sim_time': use_sim_time}],
-                    arguments=[urdf]),
+                    arguments=[urdf_fixed],
+                    condition=IfCondition(fixed)
+                    ),
+            Node(
+                    package='robot_state_publisher',
+                    executable='robot_state_publisher',
+                    name='robot_state_publisher',
+                    output='screen',
+                    parameters=[{'use_sim_time': use_sim_time}],
+                    arguments=[urdf],
+                    condition=UnlessCondition(fixed)
+                    ),
             Node(
                     package='rviz2',
                     executable='rviz2',
