f56d48925c6a7af20724e02b40f39b73e8153e29
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-05-10 02:49:57 +0200

    Updated setup.py in anro_interpolation

--- anro_interpolation/setup.py
+++ anro_interpolation/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
-
+import os
+from glob import glob
 package_name = 'anro_interpolation'
 
 setup(
@@ -10,20 +11,21 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='gabriel',
-    maintainer_email='gabrysbrzezinski@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Gabriel Brzeziński, Kacper Nowakowski',
+    maintainer_email='gabrysbrzezinski@gmail.com, casperus99@wp.pl',
+    description="Package with forwad kinematics calulations",
+    license='MIT License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'iservice = anro_interpolation.jint_control_srv:main',
-            'iclient = anro_interpolation.jint:main',
-            'oservice = anro_interpolation.oint_control_srv:main',
-            'oclient = anro_interpolation.oint:main',
+            'jint_control_srv = anro_interpolation.jint_control_srv:main',
+            'jint = anro_interpolation.jint:main',
+            'oint_control_srv = anro_interpolation.oint_control_srv:main',
+            'oint = anro_interpolation.oint:main',
         ],
     },
 )
