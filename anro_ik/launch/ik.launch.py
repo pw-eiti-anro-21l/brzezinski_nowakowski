f5901cd0fda312ce1814344754a68f81b0cc0caf
Gabriel Brzeziński <gabrysbrzezinski@gmail.com>
2021-05-22 19:26:51 +0200

    Updated launch configurations

--- anro_ik/launch/ik.launch.py
+++ anro_ik/launch/ik.launch.py
@@ -6,6 +6,11 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
             Node(
+                    package='anro_interpolation',
+                    executable='oint_control_srv',
+                    name='OINT_CONTROL_SRV',
+                    output='screen'),
+            Node(
                     package='anro_ik',
                     executable='state_publisher',
                     name='state_publisher',
